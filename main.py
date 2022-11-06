3ff907ec9d398f36a2ef534d4886599904fe0041
--replace-all <56081843+bone0670@users.noreply.github.com>
2022-11-06 12:01:35 -0500

    Set default public credentials + UI cleanup

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import PySimpleGUI as sg
 from spotdl import Spotdl
 
+DEFAULT_CONFIG = {
+    "client_id": "5f573c9620494bae87890c0f08a60293",
+    "client_secret": "212476d9b0f3472eaa762d90b19b0ba8",
+}
+
+spotdl = Spotdl(client_id=DEFAULT_CONFIG["client_id"],
+                client_secret=DEFAULT_CONFIG["client_secret"],
+                )
+
 layout =    [[sg.Text('Enter Youtube URL:')],
             [sg.InputText(key='YT_URL')],
             [sg.Text('Enter Spotify URL:')],
@@ -9,14 +18,21 @@ layout =    [[sg.Text('Enter Youtube URL:')],
 
 window = sg.Window('Music Downloader', layout)
 
-event, values = window.read()
+while True:
+    event, values = window.read()
+    if(event == 'Cancel' or event == sg.WIN_CLOSED):
+        break
+    
+    youtubeURL = values['YT_URL']
+    spotifyURL = values['SPOT_URL']
+
+    if(event == 'Submit' and (not values['YT_URL'] or not values['SPOT_URL'])):
+        sg.popup('Please specify a Youtube and Spotify URL')
+    else:
+        search  = youtubeURL + "|" + spotifyURL
+
+        song = spotdl.search([search])
+        spotdl.download(song[0])
+        sg.popup("Finished.")
+
 window.close()
-
-youtubeURL = values['YT_URL']
-spotifyURL = values['SPOT_URL']
-
-search  = youtubeURL + "|" + spotifyURL
-
-spotdl = Spotdl(client_id='a', client_secret='a')
-song = spotdl.search([search])
-spotdl.download(song)
